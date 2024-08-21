2885baa668d20ba44c0a832f83d6b47ab95f42e6
Frederico Barbosa Muniz <fbamuniz@servidor.acad>
2024-08-21 11:57:23 -0300

    t2 tds 1

--- php8-300-mvc-bootstrap/php8-30-mvc-bootstrap-bkp/controller/funcao-create-aluno.php
+++
@@ -1,5 +0,0 @@
-<?php
-  include("../model/connect.php");
-  mysqli_query($conexao,"INSERT INTO alunos (Aluno_Nome, Aluno_Cidade, Aluno_Curso) VALUES ('".$_POST["campo_nome"]."','".$_POST["campo_cidade"]."','".$_POST["campo_curso"]."')");
-  header("location:../view/");
-?>
--- php8-300-mvc-bootstrap/php8-30-mvc-bootstrap-bkp/controller/funcao-delete-aluno.php
+++
@@ -1,5 +0,0 @@
-<?php
-include("../model/connect.php");
-mysqli_query($conexao, "DELETE FROM alunos WHERE Aluno_Codigo = ".$_GET["ida"]);
-header("location:../view/");
-?>

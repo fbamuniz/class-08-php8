ce4a759f7ae609db29a91a2879fb15c0fdec3e36
Fred <fbamuniz@servidor.acad>
2024-09-05 13:21:40 -0300

    v1

--- php8-14-array/php8-14_09-array-splice/index.php
+++ php8-14-array/php8-14_09-array-splice/index.php
@@ -15,7 +15,7 @@
     // Criando um array com nomes de cidades
     $cidades = array("São Paulo", "Rio de Janeiro", "Belo Horizonte", "Salvador");
 
-    // Removendo o elemento "Belo Horizonte" do array. Se fosse pra remover São Paulo e Rio de Janeiro, logo seria: array_splice($cidades, 0, 2);
+    // Removendo o elemento "Belo Horizonte" do array. Se fosse pra remover São Paulo e Rio de Janeiro, logo seria: array_splice($cidades, 0, 1);
     array_splice($cidades, 2, 1);
 
     // Imprimindo o array atualizado
